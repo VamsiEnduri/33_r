93028c6a840e8d3f5b068707aad42c9740e3088e
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-05-03 15:55:13 +0530

    dlete opeartion

--- j_server/index.js
+++ j_server/index.js
@@ -1,6 +1,9 @@
 let devloperBtn=document.getElementById("devsBtn")
 let devlopersTypesBtnContainer=document.getElementById("devlopersTypesBtnContainer")
 
+const devs_Url="http://localhost:5000"
+const testing_Url="http://localhost:5000"
+
 devlopersTypesBtnContainer.style.marginTop="2rem"
 devloperBtn.addEventListener("click",async()=>{
 
@@ -11,39 +14,90 @@ devloperBtn.addEventListener("click",async()=>{
     <span class="devTypeBtn">fullstackDevs</span>
     `
 
-   const res=await fetch("http://localhost:3000/developers")
-   const data=await res.json()
-   console.log(data)
+  
 
    const specificCatDevBtn=devlopersTypesBtnContainer.querySelectorAll(".devTypeBtn")
    console.log(specificCatDevBtn)
    
 
    specificCatDevBtn.forEach((btn)=>{
-    let catWiseDevsContainer=document.createElement("div");
-        catWiseDevsContainer.innerHTML=""
+  
       console.log("btn",btn)
-      btn.addEventListener("click",()=>{
+      btn.addEventListener("click",async()=>{
         const btnTxt=btn.innerHTML.slice(0,-1);
         console.log(btnTxt)
-        const filteredData=data[btnTxt]
-        console.log(filteredData)
+        const res=await fetch(`${devs_Url}/${btnTxt}`)
+        const data=await res.json()
+        console.log(data)
+        const filteredData=data
+        console.log(filteredData,"fil data" )
 
-       
-       
-
+        let catWiseDevsContainer=document.getElementById("catWiseDevsContainer")
+        catWiseDevsContainer.innerHTML=""
+console.log(catWiseDevsContainer)
         filteredData.forEach((devloper)=>{
-         
+          console.log(devloper.skills,"skills")
+
+          let skillsContainer=document.createElement("div");
+          skillsContainer.id="skillsContainer"
+
+          devloper.skills.forEach((skill)=>{
+            let spanSkill=document.createElement("span")
+            spanSkill.innerHTML=skill;
+            spanSkill.id="skill"
+
+            skillsContainer.append(spanSkill)
+          })
+
+          console.log(skillsContainer,"stred skills")
+
+
             let devDiv=document.createElement("div");
+            devDiv.id="devDiv"
         devDiv.innerHTML=`
+        <div style="display:flex;justify-content:space-between;align-items:center">
         <h1>${devloper.name}</h1>
+        <i class="fa-solid fa-trash" style="color:red;"></i>
+        </div>
+        <p>${devloper.role}</p>
+        <p>${devloper.email}</p>
+      
         `
+let dlteBtn=devDiv.querySelector(".fa-trash")
+dlteBtn.addEventListener("click",()=>{
+        const btnTxt=btn.innerHTML.slice(0,-1);
+  deleteDeveloper(devloper.id,btnTxt)  // 5000/frontendDev/4
+})
+
+        devDiv.append(skillsContainer)
+        
         catWiseDevsContainer.append(devDiv)
         })
-         
-        document.body.append(catWiseDevsContainer)
-        //  catWiseDevsContainer.innerHTML=""
+        console.log(catWiseDevsContainer)
       })
    })
 
 })
+
+
+async function deleteDeveloper(id,endPoint){
+  try{
+    let res=await fetch(`${devs_Url}/${endPoint}/${id}`,{
+      method:"DELETE"
+     })
+     console.log(res)
+
+     if(res.ok){
+      displayDevs()
+      alert("successfully dleted!!!")
+     }else{
+      alert("dletion failed")
+     }
+  }
+  catch(err){
+    console.log(err)
+  }
+   
+}
+
+
